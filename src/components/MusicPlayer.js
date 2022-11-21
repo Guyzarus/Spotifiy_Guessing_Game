7e0c82ea2424592e4ed7e380cb75be6e590c7a20
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-21 18:20:00 -0500

    Initialized music player

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,38 +1,43 @@
 import React from "react";
 import styled from "styled-components";
+import AudioPlayer from "react-h5-audio-player";
+
+const Player = () => (
+  <AudioPlayer
+    autoPlay
+    src="http://example.com/audio.mp3"
+    onPlay={(e) => console.log("onPlay")}
+  />
+);
 
 const StyledMusicPlayer = styled.div`
-    display: grid;
-    flex-direction: row;
-    align-content: center;
-    margin-top: 20px;
-    padding-bottom: 20px;
-    max-width: 500px;
-    align-items: center;
-    border: 1px solid #eeeeee;
-    box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.07);
-    width: 100%;
-    height: 130px;
-    grid-template-columns: 3;
-    grid-gap: 1rem;
-    min-width: 100%;
-    height: 100%;
-    padding-bottom: 2rem;
-    justify-content: space-evenly;
+  display: grid;
+  flex-direction: row;
+  align-content: center;
+  margin-top: 20px;
+  padding-bottom: 20px;
+  max-width: 500px;
+  align-items: center;
+  border: 1px solid #eeeeee;
+  box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.07);
+  width: 100%;
+  height: 130px;
+  grid-template-columns: 3;
+  grid-gap: 1rem;
+  min-width: 100%;
+  height: 100%;
+  padding-bottom: 2rem;
+  justify-content: space-evenly;
 
-    h2 {
-        text-align: center;
-        grid-column: 1 / 4;
-        margin-bottom: 0.5rem;
-    }
-`
+  h2 {
+    text-align: center;
+    grid-column: 1 / 4;
+    margin-bottom: 0.5rem;
+  }
+`;
 
-const MusicPlayer = ({children}) => {
-    return (
-        <StyledMusicPlayer>
-            {children}
-        </StyledMusicPlayer>
-    )
-}
+const MusicPlayer = ({ children }) => {
+  return <StyledMusicPlayer>{children}</StyledMusicPlayer>;
+};
 
-export default MusicPlayer
+export default MusicPlayer;
