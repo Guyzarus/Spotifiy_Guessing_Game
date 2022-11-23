b5850e20401d2ae21b915172fd1f40051d3446d4
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-22 23:18:29 -0500

    Attempt to implement media player code

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,35 +1,72 @@
 import React, { useState } from "react";
+// import styled from "styled-components";
+import { AudioPlayerProvider } from "react-use-audio-player";
+import { useAudioPlayer } from "react-use-audio-player";
 
-import styled from "styled-components";
-
-const StyledMusicPlayer = styled.div`
-  display: grid;
-  flex-direction: row;
-  align-content: center;
-  margin-top: 20px;
-  padding-bottom: 20px;
-  max-width: 500px;
-  align-items: center;
-  border: 1px solid #eeeeee;
-  box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.07);
-  width: 100%;
-  height: 130px;
-  grid-template-columns: 3;
-  grid-gap: 1rem;
-  min-width: 100%;
-  height: 100%;
-  padding-bottom: 2rem;
-  justify-content: space-evenly;
-
-  h2 {
-    text-align: center;
-    grid-column: 1 / 4;
-    margin-bottom: 0.5rem;
-  }
-`;
-
-const MusicPlayer = ({ children }) => {
-  return <StyledMusicPlayer>{children}</StyledMusicPlayer>;
+const App = () => {
+  return (
+    <AudioPlayerProvider>
+      <AudioPlayer file="CharlieLastNameWilson.mp3" />
+    </AudioPlayerProvider>
+  );
 };
 
-export default MusicPlayer;
+const AudioPlayer = ({ file }) => {
+  const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
+    src: file,
+    format: "mp3",
+    autoplay: false,
+    onend: () => console.log("sound has ended!"),
+  });
+
+  if (!ready && !loading) return <div>No audio to play</div>;
+  if (loading) return <div>Loading audio</div>;
+
+  return (
+    <div>
+      <button onClick={togglePlayPause}>{playing ? "Pause" : "Play"}</button>
+    </div>
+  );
+};
+
+const PlayBar = () => {
+  const { percentComplete, duration, seek } = useAudioPosition({ highRefreshRate: true })
+  
+  const goToPosition = React.useCallback((percentage) => {
+      seek(duration * percentage)
+  }, [duration, seek])
+
+  return <ProgressBar percentComplete={percentComplete} onBarPositionClick={goToPosition} />
+}
+
+export default AudioPlayer;
+// const StyledMusicPlayer = styled.div`
+//   display: grid;
+//   flex-direction: row;
+//   align-content: center;
+//   margin-top: 20px;
+//   padding-bottom: 20px;
+//   max-width: 500px;
+//   align-items: center;
+//   border: 1px solid #eeeeee;
+//   box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.07);
+//   width: 100%;
+//   height: 130px;
+//   grid-template-columns: 3;
+//   grid-gap: 1rem;
+//   min-width: 100%;
+//   height: 100%;
+//   padding-bottom: 2rem;
+//   justify-content: space-evenly;
+
+//   h2 {
+//     text-align: center;
+//     grid-column: 1 / 4;
+//     margin-bottom: 0.5rem;
+//   }
+// `;
+
+// const MusicPlayer = ({ children }) => {
+//   return <StyledMusicPlayer>{children}</StyledMusicPlayer>;
+// };
+
