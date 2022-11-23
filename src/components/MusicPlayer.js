f04bc97939e673a13aa877be95c19bf085d833a2
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-23 00:01:32 -0500

    test

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -2,25 +2,6 @@ import React, { useState } from "react";
 
 import styled from "styled-components";
 
-const AudioPlayer = ({ file }) => {
-  const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
-    src: file,
-    format: "mp3",
-    autoplay: false,
-    onend: () => console.log("sound has ended!"),
-  });
-
-  if (!ready && !loading) return <div>No audio to play</div>;
-    margin-bottom: 1rem;
-    font-size: 4rem;
-  }
-
-  @media (min-width: 70em) {
-    h2 {
-      font-size: 1.5rem;
-    if (loading) return    }
- <div>Loading audio</div>;
-
 const MusicPlayer = ({ children }) => {
   return <StyledMusicPlayer>{children}</StyledMusicPlayer>;
 };
